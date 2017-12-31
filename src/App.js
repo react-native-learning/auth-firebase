a62a6d95584b1f433a2e4ffd010f150bd7f15f52
Doan The Anh <doantheanh172tb@gmail.com>
2017-12-31 19:07:11 +0700

    [firebase auth] setting firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common'
 
+import firebase from 'firebase'
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBXpPjDs7UNp2ydi2vAr9VHmbtepq1Xs-c",
+            authDomain: "react-native-2b57f.firebaseapp.com",
+            databaseURL: "https://react-native-2b57f.firebaseio.com",
+            projectId: "react-native-2b57f",
+            storageBucket: "",
+            messagingSenderId: "1033782716030"
+        });
+    }
+
     render() {
         return (
             <View>
