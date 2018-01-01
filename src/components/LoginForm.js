abfe374b98a39d07ef57f87ed07d82b333bc6aed
Doan The Anh <doantheanh172tb@gmail.com>
2018-01-01 10:22:46 +0700

    [form login] hanlde onSuccess or onFail

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,12 +18,27 @@ class FormLogin extends Component {
         this.setState({ error: '', loading: true });
 
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch(() => {
-                        this.setState({ error: 'Authentication Failed.', loading: false });
-                    });
+                    .catch(this.onLoginFail.bind(this));
             });
+    }
+
+    onLoginSuccess() {
+        this.setState({
+            email: '',
+            password: '',
+            error: '',
+            loading: ''
+        })
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed.',
+            loading: false
+        });
     }
 
     renderButton() {
